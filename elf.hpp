26481e17a7b736aef3fd4b3e1d419ff3b846c107
Elias Aebi <eyelash@users.noreply.github.com>
2026-04-13 08:11:12 +0200

    elf: symbols

--- elf.hpp
+++ elf.hpp
@@ -20,10 +20,54 @@ public:
 		return data.size();
 	}
 	std::uint32_t add(const StringView& s) {
+		if (s.empty()) {
+			return 0;
+		}
 		const std::uint32_t index = data.size();
 		data.insert(data.end(), s.begin(), s.end());
 		data.push_back('\0');
 		return index;
+	}
+};
+
+class Symbol {
+public:
+	enum class Type: unsigned char {
+		NOTYPE,
+		OBJECT,
+		FUNC
+	};
+	enum class Binding: unsigned char {
+		LOCAL,
+		GLOBAL,
+		WEAK
+	};
+	static constexpr unsigned char get_info(Type type, Binding binding) {
+		return static_cast<unsigned char>(type) | (static_cast<unsigned char>(binding) << 4);
+	}
+	std::uint32_t name;
+	std::uint32_t position;
+	std::uint32_t size;
+	unsigned char info;
+	constexpr Symbol(std::uint32_t name, std::uint32_t position, std::uint32_t size, Type type, Binding binding = Binding::LOCAL): name(name), position(position), size(size), info(get_info(type, binding)) {}
+};
+
+class SymbolTable {
+public:
+	StringTable string_table;
+	std::vector<Symbol> symbols;
+	std::size_t size() const {
+		return symbols.size();
+	}
+	void add(const StringView& name, std::uint32_t position, Symbol::Type type, Symbol::Binding binding = Symbol::Binding::LOCAL) {
+		symbols.emplace_back(string_table.add(name), position, 0, type, binding);
+	}
+	void start_symbol(const StringView& name, std::uint32_t position, Symbol::Type type, Symbol::Binding binding = Symbol::Binding::LOCAL) {
+		symbols.emplace_back(string_table.add(name), position, 0, type, binding);
+	}
+	void end_symbol(std::uint32_t position) {
+		Symbol& symbol = symbols.back();
+		symbol.size = position - symbol.position;
 	}
 };
 
@@ -33,7 +77,9 @@ class Assembler {
 	static constexpr std::uint16_t ELF_HEADER_SIZE = 52;
 	static constexpr std::uint16_t PROGRAM_HEADER_SIZE = 32;
 	static constexpr std::uint16_t SECTION_HEADER_SIZE = 40;
+	static constexpr Addr SYMBOL_SIZE = 16;
 	// program header types
+	static constexpr std::uint32_t PT_NULL = 0;
 	static constexpr std::uint32_t PT_LOAD = 1;
 	// program header flags
 	static constexpr std::uint32_t PF_X = 1 << 0;
@@ -42,11 +88,20 @@ class Assembler {
 	// section header types
 	static constexpr std::uint32_t SHT_NULL = 0;
 	static constexpr std::uint32_t SHT_PROGBITS = 1;
+	static constexpr std::uint32_t SHT_SYMTAB = 2;
 	static constexpr std::uint32_t SHT_STRTAB = 3;
 	// section header flags
 	static constexpr std::uint32_t SHF_WRITE = 1 << 0;
 	static constexpr std::uint32_t SHF_ALLOC = 1 << 1;
 	static constexpr std::uint32_t SHF_EXECINSTR = 1 << 2;
+	// symbol type
+	static constexpr unsigned char STT_NOTYPE = 0;
+	static constexpr unsigned char STT_OBJECT = 1;
+	static constexpr unsigned char STT_FUNC = 2;
+	// symbol binding
+	static constexpr unsigned char STB_LOCAL = 0 << 4;
+	static constexpr unsigned char STB_GLOBAL = 1 << 4;
+	static constexpr unsigned char STB_WEAK = 2 << 4;
 	std::vector<char> data;
 	template <class T> void write(T t) {
 		for (std::size_t i = 0; i < sizeof(T); ++i) {
@@ -63,6 +118,9 @@ class Assembler {
 	}
 	void write(const std::vector<char>& data) {
 		this->data.insert(this->data.end(), data.begin(), data.end());
+	}
+	void write(const StringTable& string_table) {
+		write(string_table.get_data());
 	}
 public:
 	const std::vector<char>& get_data() const {
@@ -110,7 +168,7 @@ public:
 		write<std::uint32_t>(name); // name
 		write<std::uint32_t>(type); // type
 		write<std::uint32_t>(flags); // flags
-		write<Addr>(0); // address
+		write<Addr>(flags & SHF_ALLOC ? VADDR + offset : 0); // address
 		write<Addr>(offset); // offset
 		write<std::uint32_t>(size); // size
 		write<std::uint32_t>(0); // link
@@ -118,19 +176,53 @@ public:
 		write<std::uint32_t>(0); // address alignment
 		write<std::uint32_t>(0); // entry size
 	}
-	void assemble(const std::vector<char>& program) {
+	void write_section_header_symtab(std::uint32_t name, Addr offset, std::uint32_t symbol_count, std::uint32_t string_table_index) {
+		write<std::uint32_t>(name); // name
+		write<std::uint32_t>(SHT_SYMTAB); // type
+		write<std::uint32_t>(0); // flags
+		write<Addr>(0); // address
+		write<Addr>(offset); // offset
+		write<std::uint32_t>(SYMBOL_SIZE * symbol_count); // size
+		write<std::uint32_t>(string_table_index); // link
+		write<std::uint32_t>(symbol_count); // info
+		write<std::uint32_t>(0); // address alignment
+		write<std::uint32_t>(SYMBOL_SIZE); // entry size
+	}
+	void write_symbol(std::uint32_t name, Addr value, std::uint32_t size, unsigned char info, std::uint16_t section_index) {
+		write<std::uint32_t>(name); // name
+		write<Addr>(value); // value
+		write<std::uint32_t>(size); // size
+		write<unsigned char>(info); // info
+		write<unsigned char>(0); // other
+		write<std::uint16_t>(section_index); // section header index
+	}
+	void assemble(const std::vector<char>& program, const SymbolTable& symbols) {
 		StringTable string_table;
 		constexpr std::uint16_t program_header_count = 1;
-		constexpr std::uint16_t section_header_count = 3;
+		constexpr std::uint16_t section_header_count = 5;
 		constexpr Addr program_offset = ELF_HEADER_SIZE + PROGRAM_HEADER_SIZE * program_header_count + SECTION_HEADER_SIZE * section_header_count;
-		write_elf_header(program_header_count, section_header_count, section_header_count - 1, VADDR + program_offset);
+		constexpr Addr entry_point = VADDR + program_offset;
+		write_elf_header(program_header_count, section_header_count, 4, entry_point);
 		write_program_header(program_offset, program.size());
-		write_section_header(0, SHT_NULL, 0, 0, 0);
-		write_section_header(string_table.add(".text"), SHT_PROGBITS, SHF_ALLOC | SHF_EXECINSTR, program_offset, program.size());
-		auto strtab = string_table.add(".strtab");
-		write_section_header(strtab, SHT_STRTAB, 0, program_offset + program.size(), string_table.size());
+		{
+			Addr offset = program_offset;
+			write_section_header(0, SHT_NULL, 0, 0, 0);
+			write_section_header(string_table.add(".text"), SHT_PROGBITS, SHF_ALLOC | SHF_EXECINSTR, offset, program.size());
+			offset += program.size();
+			write_section_header_symtab(string_table.add(".symtab"), offset, 1 + symbols.size(), 3);
+			offset += SYMBOL_SIZE * (1 + symbols.size());
+			write_section_header(string_table.add(".strtab"), SHT_STRTAB, 0, offset, symbols.string_table.size());
+			offset += symbols.string_table.size();
+			auto shstrtab = string_table.add(".shstrtab");
+			write_section_header(shstrtab, SHT_STRTAB, 0, offset, string_table.size());
+		}
 		write(program);
-		write(string_table.get_data());
+		write_symbol(0, 0, 0, 0, 0);
+		for (const Symbol& symbol: symbols.symbols) {
+			write_symbol(symbol.name, entry_point + symbol.position, symbol.size, symbol.info, 1);
+		}
+		write(symbols.string_table);
+		write(string_table);
 	}
 	void write_file(const char* path) {
 		std::ofstream file(path);
